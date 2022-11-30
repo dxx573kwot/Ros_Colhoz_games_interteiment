c43e8ca0615ab84c7751fbc73b7493925e493736
dxx753k <aleksdminecraft@gmail.com>
2022-11-30 20:38:30 +0300

    update 0.2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ er = 1
 y, sr = librosa.load(audio_data)
 print(type(y), type(sr))
 y_harmonic, y_percussive = librosa.effects.hpss(y)
-tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr, units="time", start_bpm=bits_in_minute)
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr, units="time", start_bpm=bits_in_minute, trim=True)
 print('Detected Tempo: ' + str(tempo) + ' beats/min')
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 beat_time_diff = np.ediff1d(beat_times)
