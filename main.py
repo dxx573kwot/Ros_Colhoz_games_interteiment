40be647bab0656f99cc1ade697e5dab3a1351bce
dxx753k <aleksdminecraft@gmail.com>
2022-11-30 19:57:49 +0300

    update 0

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+
+import librosa
+import matplotlib
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+import pygame
+pygame.init()
+audio_data = 'file.wav'
+pygame.mixer.music.load(audio_data)
+tr = 0
+y, sr = librosa.load(audio_data)
+print(type(y), type(sr))
+y_harmonic, y_percussive = librosa.effects.hpss(y)
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr, units="time")
+print('Detected Tempo: ' + str(tempo) + ' beats/min')
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+beat_time_diff = np.ediff1d(beat_times)
+beat_nums = np.arange(1, np.size(beat_times))
+pygame.mixer.music.play()
+for i in beat_frames:
+    # print(i, "секунда")
+    time.sleep(i - tr)
+    print("BIT")
+    tr = i
+    print()
