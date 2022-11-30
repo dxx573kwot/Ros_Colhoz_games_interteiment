5a02b5fbb70392f8761bc79cd2facff42261704e
dxx753k <aleksdminecraft@gmail.com>
2022-11-30 20:12:14 +0300

    update 0.1

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ pygame.init()
 audio_data = 'file.wav'
 pygame.mixer.music.load(audio_data)
 tr = 0
+bits_in_minute = 60.0
+er = 1
 y, sr = librosa.load(audio_data)
 print(type(y), type(sr))
 y_harmonic, y_percussive = librosa.effects.hpss(y)
-tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr, units="time")
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr, units="time", start_bpm=bits_in_minute)
 print('Detected Tempo: ' + str(tempo) + ' beats/min')
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 beat_time_diff = np.ediff1d(beat_times)
@@ -24,6 +26,7 @@ pygame.mixer.music.play()
 for i in beat_frames:
     # print(i, "секунда")
     time.sleep(i - tr)
-    print("BIT")
+    print("BIT", er)
     tr = i
+    er += 1
     print()
