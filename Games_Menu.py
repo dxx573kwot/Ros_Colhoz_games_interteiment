f2d5db5567984380487873c2776c438ba5e73294
Smart developer <developer@yandex.ru>
2023-01-04 18:42:28 +0300

    update 0.4.0.1 + READ_ME.txt, добавлен олег(нужно сделать пасхалку с ним) дубль 2

--- Games_Menu.py
+++ Games_Menu.py
@@ -344,7 +344,7 @@ if __name__ == '__main__':
                [["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"]],
                [["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"], ["s"]]]
 
-    fullscreen = False
+    fullscreen = True
 
     pygame.init()
     SIZE = WIDTH, HEIGHT = 1250, 800
