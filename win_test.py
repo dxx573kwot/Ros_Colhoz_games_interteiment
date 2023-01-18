a53e455ef03fa26cbb3f43a8c1911756b657eff5
Smart developer <developer@yandex.ru>
2023-01-18 17:29:19 +0300

    update 0.4.0.5.3.2.2 порог в 1гб перейдён!

--- win_test.py
+++ win_test.py
@@ -1,20 +1,16 @@
 import pygame
+a = "qwertyuiopasdfghjklzxcvbnm1234567890"
+b = 0
+c = {
+
+}
+print(a[:-1])
 SIZE = WIDTH, HEIGHT = 1250, 800
 screen = pygame.display.set_mode(SIZE)
-a = 0
-b = 0
-c = 0
-while True:
-    if a >= 255:
-        a = 0
-        b += 1
-    if b >= 255:
-        b = 0
-        c += 1
-    if c >= 255:
-        a = 0
-        b = 0
-        c = 0
-    a += 1
-    screen.fill((a, b, c))
-    pygame.display.flip()
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            print(event.key)
