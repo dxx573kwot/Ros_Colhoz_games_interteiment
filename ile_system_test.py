211efc9cd309550f19efeb15e9f244a352c96237
Kripipast <fedosova.natalya2015@gmail.com>
2023-01-25 16:36:17 +0300

    Cleaning unnecessary files.

--- ile_system_test.py
+++
@@ -1,7 +0,0 @@
-f = open('text.txt', 'w')
-l = [1, 2, 3, 4, 5]
-for index in l:
-    f.write("Витя кислый" + "///" + str(index) + '\n')
-f.close()
-f2 = open('text.txt', 'r')
-print(f2.read().split("\n"))
